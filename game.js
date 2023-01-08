49c9149ef1768184e0f4c88ee97b4ce677b90c68
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-01-08 13:01:54 -0300

    asd

--- game.js
+++ game.js
@@ -7,34 +7,6 @@ let elementsSize;
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
 
-function startGame() {
-  console.log({canvasSize, elementsSize});
-
-  game.font = elementsSize + 'px Verdana';
-  game.textAlign = 'end';
-
-  const map = maps[2];
-  const mapRows = map.trim().split('\n');
-  const mapRowCols = mapRows.map(row => row.trim().split(''));
-  console.log({map, mapRows, mapRowCols });
-
-  mapRowCols.forEach((row, rowI) => {
-    row.forEach((col, colI) => {
-      const emoji = emojis[col];
-      const posX = elementsSize * (colI + 1);
-      const posY = elementsSize * (rowI + 1);
-      game.fillText(emoji, posX, posY);
-      console.log({ row, rowI, col, colI});
-    });
-  });
-
-  // for (let row = 1; row <= 10; row++) {
-  //   for (let col = 1; col <= 10; col++) {
-  //   game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
-  //   }
-  // }
-}
-
 function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
     canvasSize = window.innerWidth * 0.8;
@@ -49,3 +21,23 @@ function setCanvasSize() {
 
   startGame();
 }
+
+function startGame() {
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  const map = maps[2];
+  const mapRows = map.trim().split('\n');
+  const mapRowCols = mapRows.map(row => row.trim().split(''));
+
+  mapRowCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
+      game.fillText(emoji, posX, posY);
+    });
+  });
+}
+
+.addEventListener
