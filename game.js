3a3be863fab0e3969a3509647fe317388947f2bc
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-01-14 11:51:59 -0300

    conteo de vidas

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ const btnDown = document.querySelector('#down');
 let canvasSize;
 let elementsSize;
 let level = 0;
+let lives = 3;
 
 const playerPosition = {
   x: undefined,
@@ -97,7 +98,7 @@ function movePlayer() {
   });
 
   if (enemyCollision) {
-    console.log('Chocaste contra un enemigo!');
+    levelFail();
   }
 
   game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
@@ -106,6 +107,24 @@ function movePlayer() {
 function levelWin() {
   console.log('Pasaste de nivel!');
   level++;
+  startGame();
+}
+
+function levelFail() {
+  console.log('Chocaste contra un enemigo');
+
+  lives--;
+
+  console.log(lives);
+
+  if (lives <= 0) {
+    level = 0;
+    lives = 3;
+
+  }
+
+  playerPosition.x = undefined;
+  playerPosition.y = undefined;
   startGame();
 }
 
